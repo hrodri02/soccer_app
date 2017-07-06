7ac6f47717fb3fc957e1cf7f47b5165a156b67c2
Brayan Rodriguez <brayanrodriguez@pcp076280pcs.wireless.calpoly.edu>
2017-07-06 09:54:35 -0700

    Initial Commit

---
+++ user_list_viewTests/user_list_viewTests.swift
@@ -0,0 +1,36 @@
+//
+//  user_list_viewTests.swift
+//  user_list_viewTests
+//
+//  Created by Brayan Rodriguez  on 7/6/17.
+//  Copyright © 2017 Brayan Rodriguez. All rights reserved.
+//
+
+import XCTest
+@testable import user_list_view
+
+class user_list_viewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
